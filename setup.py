8f8c3c66adc288d068bb3360c72a2deba2774af5
Berdache Adyezik <adyezik@gmail.com>
2015-09-06 07:04:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-# Dynamically calculate the version based on pyperclip.VERSION.
+
 setup(
     name='Xclipboard',
     version='1.1',
@@ -10,7 +10,7 @@ setup(
     author_email='adyezik@gmail.com',
     description=('simple python3 clipboard module based on tkinter croos-platform'),
     license='MIT',
-    py_modules=['Xclipboard']
+    py_modules=['Xclipboard'],
     keywords="gui tkinter test module keyboard copy paste clear clear xclipboard control",
     classifiers=[
        ' Classifier: Development Status :: 5 - Production/Stable'
@@ -19,5 +19,5 @@ setup(
        ' Classifier: Environment :: MacOS X'
         'Classifier: Intended Audience :: Developers'
        ' Classifier: Operating System :: OS Independent'
-    ],
+    ]
 )
